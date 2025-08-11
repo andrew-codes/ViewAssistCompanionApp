8928e7e398c2eb013e0a776e8642348621635af7
Mark Parker <msparker@sky.com>
2025-08-11 12:07:25 +0100

    v0.3.4b1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.3.4b0"
+        versionName = "0.3.4b1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "vaca-$versionName")
     }
