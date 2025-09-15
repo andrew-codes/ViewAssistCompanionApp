d1e0cc7e7930e0712efe11b200363d4925f9c02e
Mark Parker <msparker@sky.com>
2025-09-15 15:47:27 +0100

    v0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.4.1"
+        versionName = "0.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
