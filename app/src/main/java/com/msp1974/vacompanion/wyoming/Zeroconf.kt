4517b2cc888015f9dc00208df179bf32c96e5467
Mark Parker <msparker@sky.com>
2025-11-25 13:17:53 +0000

    refactor: improve code flow

--- app/src/main/java/com/msp1974/vacompanion/wyoming/Zeroconf.kt
+++ app/src/main/java/com/msp1974/vacompanion/wyoming/Zeroconf.kt
@@ -4,61 +4,61 @@ import android.content.Context
 import android.net.nsd.NsdManager
 import android.net.nsd.NsdServiceInfo
 import com.msp1974.vacompanion.settings.APPConfig
+import timber.log.Timber
 
 internal class Zeroconf(private val context: Context) {
     private var nsdManager: NsdManager? = null
-    private var registrationListener: NsdManager.RegistrationListener? = null
     private val config = APPConfig.Companion.getInstance(context)
     private var isRegistered: Boolean = false
     var serviceName: String? = null
 
-    init {
-        initializeRegistrationListener()
-        nsdManager = context.getSystemService(Context.NSD_SERVICE) as NsdManager
-    }
-
     fun registerService(port: Int) {
         if (!isRegistered) {
-            val serviceInfo = NsdServiceInfo()
-            serviceInfo.serviceName = "vaca-${config.uuid}"
-            serviceInfo.serviceType = "_vaca._tcp."
-            serviceInfo.port = port
+            val serviceInfo = NsdServiceInfo().apply {
+                serviceName = "vaca-${config.uuid}"
+                serviceType = "_vaca._tcp."
+                setPort(port)
+            }
 
-            nsdManager!!.registerService(
-                serviceInfo, NsdManager.PROTOCOL_DNS_SD, registrationListener
-            )
+            try {
+                nsdManager = (context.getSystemService(Context.NSD_SERVICE) as NsdManager).apply {
+                    registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, registrationListener)
+                }
+            } catch (e: Exception) {
+                Timber.e("Error registering NSD service: $e")
+            }
         }
     }
 
     fun unregisterService() {
-        if (isRegistered) {
+        if (isRegistered && nsdManager != null) {
             nsdManager!!.unregisterService(registrationListener)
         }
     }
 
-    fun initializeRegistrationListener() {
-        registrationListener = object : NsdManager.RegistrationListener {
-            override fun onServiceRegistered(nsdServiceInfo: NsdServiceInfo) {
-                // Save the service name. Android may have changed it in order to
-                // resolve a conflict, so update the name you initially requested
-                // with the name Android actually used.
-                serviceName = nsdServiceInfo.serviceName
-                isRegistered = true
-            }
+    private val registrationListener = object : NsdManager.RegistrationListener {
+        override fun onServiceRegistered(nsdServiceInfo: NsdServiceInfo) {
+            // Save the service name. Android may have changed it in order to
+            // resolve a conflict, so update the name you initially requested
+            // with the name Android actually used.
+            serviceName = nsdServiceInfo.serviceName
+            isRegistered = true
+        }
 
-            override fun onRegistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
-                // Registration failed! Put debugging code here to determine why.
-            }
+        override fun onRegistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
+            // Registration failed! Put debugging code here to determine why.
+            Timber.e("Failed to register NSD service: $errorCode")
+        }
 
-            override fun onServiceUnregistered(arg0: NsdServiceInfo) {
-                // Service has been unregistered. This only happens when you call
-                // NsdManager.unregisterService() and pass in this listener.
-                isRegistered = false
-            }
+        override fun onServiceUnregistered(arg0: NsdServiceInfo) {
+            // Service has been unregistered. This only happens when you call
+            // NsdManager.unregisterService() and pass in this listener.
+            isRegistered = false
+        }
 
-            override fun onUnregistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
-                // De-registration failed. Put debugging code here to determine why.
-            }
+        override fun onUnregistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
+            // De-registration failed. Put debugging code here to determine why.
+            Timber.e("Failed to unregister NSD service: $errorCode")
         }
     }
 }
