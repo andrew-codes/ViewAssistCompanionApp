17ab0b979499f5d59ce9e5c0a13aea8519e5af33
Mark Parker <msparker@sky.com>
2025-12-29 17:02:09 +0000

    added: device admin and  screen lock

---
+++ app/src/main/java/com/msp1974/vacompanion/DeviceAdminReceiver.kt
@@ -0,0 +1,13 @@
+package com.msp1974.vacompanion
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class VACADeviceAdminReceiver : DeviceAdminReceiver() {
+    override fun onEnabled(context: Context, intent: Intent) {
+        super.onEnabled(context, intent)
+        Toast.makeText(context, "Admin is enabled", Toast.LENGTH_SHORT).show()
+    }
+}
